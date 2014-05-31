b88b448ca46a557ec5e73022f0e883b8f6f8019f
Albert Zhang <zhgwenming@gmail.com>
2014-05-31 16:29:15 +0800

    vnet network information

--- cmd/virt_template.go
+++ cmd/virt_template.go
@@ -5,18 +5,47 @@
 package main
 
 import (
+	"log"
+	"net"
+	"os"
 	"text/template"
 )
 
 const VirtNetTemplate = `
 <network>
-  <name>{{.Network.Name}}</name>
+  <name>{{.Name}}</name>
   <forward mode="bridge">
-    <interface dev="{{.Network.Iface.Name}}"/>
+    <interface dev="{{.Iface.Name}}"/>
   </forward>
 </network>
 `
 
-func init() {
-	netxml, err := template.New("net").Parse(VirtNetTemplate)
+type Network struct {
+	Name  string
+	Iface *net.Interface
+}
+
+var (
+	networks = make([]*Network, 0, 2)
+)
+
+func main() {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, iface := range ifaces {
+		if iface.Flags&(net.FlagLoopback|net.FlagPointToPoint) == 0 {
+			ifi := iface
+			network := &Network{"vnet-" + ifi.Name, &ifi}
+			networks = append(networks, network)
+			log.Printf("%s", iface.Name)
+		}
+	}
+
+	xml := template.Must(template.New("net").Parse(VirtNetTemplate))
+	for _, net := range networks {
+		xml.Execute(os.Stdout, net)
+	}
 }
