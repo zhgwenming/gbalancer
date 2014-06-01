5db451a5789e8a3086cbd9b68e2c0ca52208699b
Albert Zhang <zhgwenming@gmail.com>
2014-06-01 20:57:38 +0800

    leader lock function

--- cmd/ldirector/main.go
+++ cmd/ldirector/main.go
@@ -7,10 +7,45 @@ package main
 import (
 	"github.com/coreos/go-etcd/etcd"
 	"log"
+	"os"
+	"strconv"
+	"time"
 )
 
-func main() {
+const (
+	ttl = 60
+)
+
+func BecomeLeader(cl *etcd.Client, ttl uint64, sleeptime time.Duration) {
 	cluster := "ldirector"
+	value := strconv.Itoa(os.Getpid())
+
+	for {
+		// curl -X PUT http://127.0.0.1:4001/mod/v2/leader/{clustername}?ttl=60 -d name=servername
+		// not supported by etcd client yet
+		// so we create a new key and ignore the return value first.
+		if _, err := cl.Create(cluster, value, ttl); err != nil {
+			time.Sleep(sleeptime * time.Second)
+		} else {
+			log.Printf("No leader exist, taking the leadership")
+			go func() {
+				for {
+					time.Sleep(sleeptime * time.Second)
+					// update the ttl periodically, should never get error
+					_, err = cl.CompareAndSwap(cluster, value, ttl, value, 0)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+			}()
+		}
+	}
+}
+
+func main() {
+	sleeptime := time.Duration(ttl / 3)
+
+	//log.Printf("Sleep time is %d", sleeptime)
 
 	server := []string{
 		"http://127.0.0.1:4001",
@@ -18,11 +53,6 @@ func main() {
 
 	cl := etcd.NewClient(server)
 
-	// curl -X PUT http://127.0.0.1:4001/mod/v2/leader/{clustername}?ttl=60 -d name=servername
-	// not supported by etcd client yet
-	// so we create a new key and ignore the return value first.
-	_, err := cl.Create(cluster, cluster, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
+	BecomeLeader(cl, ttl, sleeptime)
+
 }
