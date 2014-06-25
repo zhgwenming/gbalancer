c33df057565f9e1734d684af1f3230d72d8ae599
Albert Zhang <zhgwenming@gmail.com>
2014-06-25 19:52:21 +0800

    get listen addresses

--- config/config.go
+++ config/config.go
@@ -4,6 +4,16 @@
 
 package config
 
+import (
+	"fmt"
+	"strings"
+)
+
+type ListenAddr struct {
+	net   string
+	laddr string
+}
+
 type Configuration struct {
 	Service    string
 	ExtCommand string
@@ -12,5 +22,21 @@ type Configuration struct {
 	Addr       string
 	Port       string
 	UnixSocket string
+	Listen     []string
 	Backend    []string
+}
+
+func (c *Configuration) GetListenAddrs() ([]ListenAddr, error) {
+	laddrs := make([]ListenAddr, 0, len(c.Listen))
+	for _, l := range c.Listen {
+		protoAddrParts := strings.SplitN(l, "://", 2)
+		if len(protoAddrParts) != 2 {
+			err := fmt.Errorf("incorrect listen addr %s", l)
+			return laddrs, err
+		}
+		addr := ListenAddr{protoAddrParts[0], protoAddrParts[1]}
+		laddrs = append(laddrs, addr)
+	}
+
+	return laddrs, nil
 }
