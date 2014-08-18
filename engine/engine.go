522a4202192f79ffa90e024f39ff8edbe90dc070
Albert Zhang <zhgwenming@gmail.com>
2014-08-18 10:22:42 +0800

    native engine code cleanup

--- engine/engine.go
+++ engine/engine.go
@@ -11,7 +11,6 @@ import (
 	"github.com/zhgwenming/gbalancer/engine/native"
 	logger "github.com/zhgwenming/gbalancer/log"
 	"github.com/zhgwenming/gbalancer/wrangler"
-	"net"
 	"sync"
 )
 
@@ -19,8 +18,6 @@ var (
 	log        = logger.NewLogger()
 	ipvsMode   = flag.Bool("ipvs", false, "to use lvs as loadbalancer")
 	ipvsRemote = flag.Bool("remote", false, "independent director")
-	useTunnel  = flag.Bool("tunnel", true, "use tunnel mode")
-	failover   = flag.Bool("failover", false, "whether to enable failover mode for scheduling")
 )
 
 func Serve(settings *config.Configuration, wgroup *sync.WaitGroup) (done chan struct{}) {
@@ -44,51 +41,7 @@ func Serve(settings *config.Configuration, wgroup *sync.WaitGroup) (done chan st
 			go ipvs.LocalSchedule(status)
 		}
 	} else {
-		job := make(chan *native.Request)
-
-		// start the scheduler
-		sch := native.NewScheduler(*failover, *useTunnel)
-		go sch.Schedule(job, status)
-
-		listenAddrs, err := settings.GetListenAddrs()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for _, listenAddr := range listenAddrs {
-			listener, err := listenAddr.Listen()
-
-			// close the listener makes the unix socket file got removed
-			wgroup.Add(1)
-			go func() {
-				<-done
-				listener.Close()
-				wgroup.Done()
-			}()
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// tcp/unix listener
-			go func() {
-				for {
-					if conn, err := listener.Accept(); err == nil {
-						//log.Println("main: got a connection")
-						req := &native.Request{Conn: conn}
-						job <- req
-					} else {
-						if neterr, ok := err.(net.Error); ok && neterr.Temporary() {
-							log.Printf("%s\n", err)
-						} else {
-							// we should got a errClosing
-							log.Printf("Existing listen loop\n")
-							return
-						}
-					}
-				}
-			}()
-		}
+		native.Serve(settings, wgroup, done, status)
 	}
 	return done
 }
--- engine/native/native.go
+++ engine/native/native.go
@@ -5,9 +5,63 @@
 package native
 
 import (
+	"flag"
+	"github.com/zhgwenming/gbalancer/config"
 	logger "github.com/zhgwenming/gbalancer/log"
+	"net"
+	"sync"
 )
 
 var (
-	log = logger.NewLogger()
+	log       = logger.NewLogger()
+	useTunnel = flag.Bool("tunnel", true, "use tunnel mode")
+	failover  = flag.Bool("failover", false, "whether to enable failover mode for scheduling")
 )
+
+func Serve(settings *config.Configuration, wgroup *sync.WaitGroup, done chan struct{}, status chan map[string]int) {
+	job := make(chan *Request)
+
+	// start the scheduler
+	sch := NewScheduler(*failover, *useTunnel)
+	go sch.Schedule(job, status)
+
+	listenAddrs, err := settings.GetListenAddrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, listenAddr := range listenAddrs {
+		listener, err := listenAddr.Listen()
+
+		// close the listener makes the unix socket file got removed
+		wgroup.Add(1)
+		go func() {
+			<-done
+			listener.Close()
+			wgroup.Done()
+		}()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// tcp/unix listener
+		go func() {
+			for {
+				if conn, err := listener.Accept(); err == nil {
+					//log.Println("main: got a connection")
+					req := &Request{Conn: conn}
+					job <- req
+				} else {
+					if neterr, ok := err.(net.Error); ok && neterr.Temporary() {
+						log.Printf("%s\n", err)
+					} else {
+						// we should got a errClosing
+						log.Printf("Existing listen loop\n")
+						return
+					}
+				}
+			}
+		}()
+	}
+}
