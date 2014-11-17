04042eb81c94c8baf32ff6d87b94a39e44555cb3
Albert Zhang <zhgwenming@gmail.com>
2014-11-17 19:16:33 +0800

    supervisor WIP

---
+++ daemon/supervisor.go
@@ -0,0 +1,11 @@
+// Copyright 2014. All rights reserved.
+// Use of this source code is governed by a GPLv3
+// Author: Wenming Zhang <zhgwenming@gmail.com>
+
+// +build go1.4
+
+package daemon
+
+type Supervisor struct {
+	Daemon
+}
